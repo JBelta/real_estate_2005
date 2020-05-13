e0b2d7c9e3d14b4a1b626f8710a42dd0fd823f0c
James Belta <james.belta@gmail.com>
2020-05-13 10:16:04 -0600

    Build sort_house test

--- test/house_test.rb
+++ test/house_test.rb
@@ -47,4 +47,20 @@ class HouseTest < Minitest::Test
       assert_equal [room_4], house.rooms_from_category(:basement)
       assert_equal ({"price" => 400000, "address" => "123 sugar lane"}), house.details
   end
+
+  def test_sort_house
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+
+    assert_equal 210.53, house.price_per_square_foot
+    assert_equal [room_4, room_3, room_2, room_1], house.rooms_sorted_by_area
+    assert_equal ({:bedroom => [room_1, room_2], :living_room => room_3, :basement => room_4})
+  end
 end
