2c609c1f1117ca0f507746ef87c757be64295add
James Belta <james.belta@gmail.com>
2020-05-13 09:34:37 -0600

    Build attributes test

--- test/house_test.rb
+++ test/house_test.rb
@@ -8,4 +8,12 @@ class HouseTest < Minitest::Test
     house = House.new("$400000", "123 sugar lane")
     assert_instance_of House, house
   end
+
+  def it_has_attributes
+    house = House.new("$400000", "123 sugar lane")
+
+    assert_equal 400000, house.price
+    assert_equal "123 sugar lane", house.address
+    assert_equal []. house.rooms
+  end
 end
