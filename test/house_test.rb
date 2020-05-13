8de20e3d371719e90180c3811d7fa1fa75642c48
James Belta <james.belta@gmail.com>
2020-05-13 09:39:04 -0600

    Build has_rooms test, edit attributes test

--- test/house_test.rb
+++ test/house_test.rb
@@ -9,11 +9,21 @@ class HouseTest < Minitest::Test
     assert_instance_of House, house
   end
 
-  def it_has_attributes
+  def test_it_has_attributes
     house = House.new("$400000", "123 sugar lane")
 
     assert_equal 400000, house.price
     assert_equal "123 sugar lane", house.address
-    assert_equal []. house.rooms
+    assert_equal [], house.rooms
+  end
+
+  def test_it_has_rooms
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    house.add_room(room1)
+    house.add_room(room2)
+
+    assert_equal [room1, room2], house.rooms
   end
 end
