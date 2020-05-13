9f362002b21ec912e5a739fa24fb8588b4782482
James Belta <james.belta@gmail.com>
2020-05-13 09:44:49 -0600

    Build abouve_market_average? test

--- test/house_test.rb
+++ test/house_test.rb
@@ -26,4 +26,9 @@ class HouseTest < Minitest::Test
 
     assert_equal [room_1, room_2], house.rooms
   end
+
+  def test_above_market_average?
+    house = House.new("$400000", "123 sugar lane")
+    assert_equal false, house.above_market_average?
+  end
 end
